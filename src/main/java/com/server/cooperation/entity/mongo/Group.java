67af93f97b302d1fdbc3da8d2c74028b23befbef
boluo1230 <942261674@qq.com>
2019-04-30 11:47:17 +0800

    添加team，group表

---
+++ src/main/java/com/server/cooperation/entity/mongo/Group.java
@@ -0,0 +1,36 @@
+package com.server.cooperation.entity.mongo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Document(collection = "group")
+@Getter
+@Setter
+public class Group {
+
+    @Id
+    private String id;
+    @Field("team_id")
+    private String teamId;
+    private String title;
+    private String logo;
+    private String status;
+
+    @Field("create_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createDate;
+
+    @Field("update_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date updateDate;
+
+}
---
+++ src/main/java/com/server/cooperation/entity/mongo/Team.java
@@ -0,0 +1,38 @@
+package com.server.cooperation.entity.mongo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+/**
+ * 团队
+ */
+@Document(collection = "team")
+@Getter
+@Setter
+public class Team {
+
+    @Id
+    private String id;
+    private String name;//团队名字
+    private String description;
+    private int status;//1正常    0删除
+
+    @Field("create_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createDate;
+
+    @Field("update_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date updateDate;
+
+
+}
